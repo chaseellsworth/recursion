5b3d0891042db0fedb7e4117534c8f1c9463f56e
Chase Ellsworth <chaseellsworth@Chases-MacBook-Air.local>
2014-10-20 03:53:36 -0600

    updated

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,22 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
-};
+  var elements = document.body.getElementsByTagName("*");
+    var filtElements = [];
+    for (var i = 0; i < elements.length; i++) {
+        var classes = elements[i].classList;
+        if (classes.contains(className)){
+        filtElements.push(elements[i]);
+        }
+    }
+    return filtElements;
+    }
+
+  	// should use: 
+	  	// document.body
+	  	// element.childNodes
+	  	// element.classList probably with "contains" method
+  	// functionalities:
+  		// return collection containing all child elements which have the given class names
+  		// takes a list of class names to match (more than one)
+  		//
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,6 +2,52 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(obj, replacer) {
+
+// if they use a replacer function. Not sure how to use it with arrays or a filter  
+  	if (replacer) {
+  		obj = replacer(obj);
+    }
+// for array    	
+  	if (Array.isArray(obj)){
+      	strArray = [];
+      	for (var i = 0; i < obj.length; i++){
+        	strArray.push('"' + obj[i].toString() + '"');      
+      	}
+      	return "[" + strArray + "]";
+
+// for an object  
+  	} else if (typeof obj === "object") {
+  	var objKeys = Object.keys(obj);
+  	var keyValArray = [];
+    for (var i = 0; i < objKeys.length; i++){
+      	var keyValString = '"' + objKeys[i] + '":';
+      	var objVal = obj[objKeys[i]];
+        if (typeof objVal == "string") {
+        	keyValString = keyValString + '"' + objVal + '"';
+        } else {
+        	keyValString = keyValString + stringifyJSON(objVal);
+        }
+      	keyValArray.push(keyValString);
+    }
+    return "{" + keyValArray.join(",") + "}";      
+
+// for boolean
+  	} else if (typeof obj === "boolean"){
+    	return obj.toString();
+
+// for string
+  	} else if (typeof obj === "string"){
+    	return '"' + obj.toString() + '"';
+
+// for number
+  	} else if (typeof obj === "number"){
+    	return obj.toString();
+  	}
+
 };
+
+
+
+
+
